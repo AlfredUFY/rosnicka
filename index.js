1ac0a51e8945cc9523e12bf349606690e6ad8ac0
Alfred <105510070+AlfredUFY@users.noreply.github.com>
2023-06-09 04:51:02 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const axios = require('axios');
+
+const url = 'https://generativelanguage.googleapis.com/v1beta2/models/text-bison-001:generateText?key='+process.env.key;
+const payload = {
+    prompt: { text: 'Write a story about a magic backpack.' },
+};
+
+axios({
+    method: 'post',
+    url,
+    headers: { 'Content-Type': 'application/json' },
+    data: payload,
+})
+    .then((response) => {
+        console.log(response.data);
+    })
+    .catch((error) => {
+        console.error('Error:', error.message);
+    });
